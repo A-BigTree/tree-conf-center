6c6b55f6eeccda94bb5f75f94c2a415edd9cc317
Shuxin Wang <81904920+A-BigTree@users.noreply.github.com>
2023-08-21 01:28:10 +0800

    update nacos client

---
+++ tree-conf-center-sdk/src/main/java/top/abigtree/conf/sdk/ConfigExample.java
@@ -0,0 +1,40 @@
+package top.abigtree.conf.sdk;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+/**
+ * @author Shuxin Wang <shuxinwang662@gmail.com>
+ * Created on 2023/8/20
+ */
+public class ConfigExample {
+    public static void main(String[] args) throws InterruptedException, NacosException {
+        String serverAddr = "172.26.15.24";
+        String dataId = "tree.conf.test";
+        String group = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
+        properties.put(PropertyKeyConst.USERNAME,"nacos");
+        properties.put(PropertyKeyConst.PASSWORD,"nacos");
+        ConfigService configService = NacosFactory.createConfigService(properties);
+        String content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+        configService.addListener(dataId, group, new Listener() {
+            @Override
+            public void receiveConfigInfo(String configInfo) {
+                System.out.println("receive:" + configInfo);
+            }
+
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+        });
+    }
+}
